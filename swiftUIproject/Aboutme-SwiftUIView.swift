cb0bbac5edc3a439b631627333e5c88c3d8a5d43
Niwei Li <cecily.li.822@gmail.com>
2022-07-05 14:00:04 -0700

    first commit

---
+++ swiftUIproject/Aboutme-SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  Aboutme-SwiftUIView.swift
+//  swiftUIproject
+//
+//  Created by new on 7/5/22.
+//
+
+import SwiftUI
+
+struct Aboutme_SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Aboutme_SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        Aboutme_SwiftUIView()
+    }
+}
