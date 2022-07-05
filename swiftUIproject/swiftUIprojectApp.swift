f2dcf0a00fd92db849d02bd5d5f61a96198f8c9e
Niwei Li <cecily.li.822@gmail.com>
2022-07-05 11:12:34 -0700

    Initial Commit

---
+++ swiftUIproject/swiftUIprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIprojectApp.swift
+//  swiftUIproject
+//
+//  Created by new on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
